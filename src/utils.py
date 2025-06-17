145a647ed397b0e45da3d393a89f8db2b9f99810
milanbhan <mil.bhan@gmail.com>
2025-06-17 19:58:23 +0200

    a

--- src/utils.py
+++ src/utils.py
@@ -2,8 +2,13 @@ from thefuzz import fuzz
 
 def get_prediction_status(answers:list[str],
                         predictions:list[str],
-                        threshold:int = 70)
+                        threshold:int = 70) -> list:
     # Compute fuzzy similarity row by row
     result = answers.combine(predictions, lambda a, b: fuzz.ratio(a, b) >= threshold)
 
     return(result)
+
+def clean_explanation(explanations:list[str]) -> list[str]:
+    explanations = explanations.str.replace("\n","")
+    explanations = explanations.str.split('Let me know if').str[0].str.strip()
+    explanations = explanations.str.rstrip()
